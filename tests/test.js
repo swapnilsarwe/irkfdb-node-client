eb4b332a6b3034de676f91c01ee8c78dfae1a069
Swapnil Sarwe <swapnil.s@media.net>
2017-08-20 12:57:36 +0530

    fixed the test cases

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,23 @@
 var chai = require('chai'),
     irkfdb = require("../src/index.js"),
-    assert = chai.assert;
+    assert = chai.assert,
+    expect = chai.expect;
 
 describe('testApiWorking', function () {
-    it('checks api is working', function () {
+    it('checks api is working', function (done) {
         irkfdb.getRandomFact().then(function (data) {
-            assert.equal(data.status, "FAIL");
-            assert.isAbove(data.resultSet.length, 0, 'length should be strictly greater than 0');
-        });
-    });
-});/*
-describe('testProperCategoryFact', function () {
-    it('checks category api is working', function () {
-        irkfdb.fromCategories('geeky').getRandomFact().then(function (data) {
             assert.equal(data.status, "OK");
-            assert.equal((data.resultSet.data[0].categories).indexOf('geeky'), true);
+            assert.isAbove((data.resultSet.data).length, 0, 'length should be strictly greater than 0');
+            done();
         });
     });
 });
-*/
+describe('testProperCategoryFact', function () {
+    it('checks category api is working', function (done) {
+        irkfdb.fromCategories('geeky').getRandomFact().then(function (data) {
+            assert.equal(data.status, "OK");
+            assert.equal(((data.resultSet.data[0].categories).indexOf('geeky') > -1), true);
+            done();
+        });
+    });
+});
